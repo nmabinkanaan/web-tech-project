2807a93b78fe85d7c7909aca83df3ecb6954a666
aaalghamdi79 <103608541+aaalghamdi79@users.noreply.github.com>
2022-05-09 01:52:58 +0300

    Add files via upload

---
+++ dbcontroller.php
@@ -0,0 +1,16 @@
+<?php
+$errors = array(); 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "GameBD";
+
+$con = mysqli_connect($host,$user,$pass, $db);
+
+// if($con){
+//   echo "Connection Sucessfully";
+// }else{
+//   echo "Fail";
+// }
+
+ ?>
